3c95cfa69de55f848875946e4fe8e392637e1715
Dmitrii <MIntriy2033@yandex.ru>
2023-04-07 18:18:03 +0200

    Initial Commit

---
+++ sudokuSolver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sudokuSolver
+//
+//  Created by Dmitry Popov on 07.04.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
